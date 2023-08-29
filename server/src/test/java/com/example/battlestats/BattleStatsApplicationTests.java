bfd2a426cb4d7371b7125e4db1ac198358d07f82
ViartFelix <viart.felix2017@gmail.com>
2023-08-29 12:00:38 +0200

    Setting up project (v0.0.1)

---
+++ server/src/test/java/com/example/battlestats/BattleStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battlestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
